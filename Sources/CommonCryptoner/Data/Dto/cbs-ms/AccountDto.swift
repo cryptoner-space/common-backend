e0b335e58569755c6496563b21189863708efe9d
skibinalexander <skibinalexander@gmail.com>
2022-11-12 19:55:43 +0300

    Доработка моделей данных

---
+++ Sources/CommonCryptoner/Data/Dto/cbs-ms/AccountDto.swift
@@ -0,0 +1,23 @@
+//
+//  AccountDto.swift
+//  
+//
+//  Created by skibinalexander on 12.11.2022.
+//
+
+import Vapor
+
+extension CBS_Dto {
+    
+    public struct AccountDto {
+        
+        public struct Req: Content {
+            
+            /// Идентификатор пользователя
+            let userId: String
+            
+        }
+        
+    }
+    
+}
---
+++ Sources/CommonCryptoner/Data/Dto/cbs-ms/CBS_Dto.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by skibinalexander on 12.11.2022.
+//
+
+import Foundation
+
+public struct CBS_Dto {}
