c9bed99a060e8bafab9553732ac08b20ef89e7c0
skibinalexander <skibinalexander@gmail.com>
2022-11-13 22:41:48 +0300

    update

--- Sources/CommonCryptoner/Data/Dto/lock-ms/Lock_GenerateKeyDto.swift
+++ Sources/CommonCryptoner/Data/Dto/lock-ms/Lock_GenerateKeyDto.swift
@@ -9,24 +9,34 @@ import Vapor
 
 extension Lock_Dto {
     
-    public struct GenerateKey {
+    public struct DEK {
         
         public struct Req: Content {
             
-            /// Версия ключа
-            let versionId: String
-            
-            /// Авторизационные данные шифрования
-            let aadContext: String
-            
-            /// Алгоритм шифрования
-            let dataKeySpec: String
-            
-            /// Отображения ответа шифра в формате plaintext
-            let skipPlaintext: Bool
+            /// Пароль пользователя для шифрования
+            let password: String
             
         }
         
+        public struct YC: Content {
+            
+            public struct Req: Content {
+                
+                /// Версия ключа
+                let versionId: String
+                
+                /// Авторизационные данные шифрования
+                let aadContext: String
+                
+                /// Алгоритм шифрования
+                let dataKeySpec: String
+                
+                /// Отображения ответа шифра в формате plaintext
+                let skipPlaintext: Bool
+                
+            }
+            
+        }
         
     }
     
