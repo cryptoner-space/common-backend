71d3cc3a2b335ca36f0da3d36663f6a2fccaf4ba
skibinalexander <skibinalexander@gmail.com>
2022-11-15 00:28:04 +0300

    update

---
+++ Sources/CommonCryptoner/Data/Dto/admin-ms/Admin_SignDto.swift
@@ -0,0 +1,58 @@
+//
+//  Admin_SignDto.swift
+//  
+//
+//  Created by skibinalexander on 15.11.2022.
+//
+
+import Vapor
+
+extension Admin_Dto {
+    
+    public struct Sign {
+        
+        public struct InReq: Content {
+            
+            /// Идентификатор девайса
+            public let deviceId: UUID
+            
+            /// Username  пользователя
+            public let username: String
+            
+            /// Пароль  пользователя
+            public let password: String
+            
+        }
+        
+        public struct UpReq: Content {
+            
+            /// Идентификатор системы владения пользователем
+            public let systemId: String
+            
+            /// Идентификатор девайса
+            public let deviceId: UUID
+            
+            /// Username пользователя
+            public let username: String
+            
+            /// Пароль аккаунта
+            public let password: String
+            
+        }
+        
+    }
+    
+    public struct Res {
+        
+        /// Идентификатор девайса
+        public let deviceId: UUID
+        
+        /// Токен авторизации
+        public let accessToken: String
+        
+        /// Модель пользователя
+        public let user: User.Res
+        
+    }
+    
+}
--- Sources/CommonCryptoner/Data/Dto/admin-ms/Admin_UserDto.swift
+++ Sources/CommonCryptoner/Data/Dto/admin-ms/Admin_UserDto.swift
@@ -9,39 +9,6 @@ import Vapor
 
 extension Admin_Dto {
     
-    public struct Sign {
-        
-        public struct InReq: Content {
-            
-            /// Идентификатор девайса
-            public let deviceId: UUID
-            
-            /// Username  пользователя
-            public let username: String
-            
-            /// Пароль  пользователя
-            public let password: String
-            
-        }
-        
-        public struct UpReq: Content {
-            
-            /// Идентификатор системы владения пользователем
-            public let systemId: String
-            
-            /// Идентификатор девайса
-            public let deviceId: UUID
-            
-            /// Username пользователя
-            public let username: String
-            
-            /// Пароль аккаунта
-            public let password: String
-            
-        }
-        
-    }
-    
     public struct User {
         
         public struct Req: Content {
