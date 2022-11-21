af5e85a7c3223f9f268bbd095f63064a1c2c711b
skibinalexander <skibinalexander@gmail.com>
2022-11-21 12:09:10 +0300

    update

--- Sources/CommonCryptoner/MIddlewares/AccountMiddleware.swift
+++ Sources/CommonCryptoner/MIddlewares/AccountMiddleware.swift
@@ -5,7 +5,6 @@
 //  Created by skibinalexander on 21.11.2022.
 //
 
-import CommonCryptoner
 import CommonVapor
 import Vapor
 
@@ -28,15 +27,19 @@ extension Request {
 
 public final class AccountMiddleware: Middleware {
     
+    // MARK: - Public Init
+    
+    public init() {}
+    
     // MARK: - Private Propertioes
     
-    private(set) weak var request: Request!
+    private(set) weak var req: Request!
     private(set) var env: Environment!
     
     // MARK: - Middleware Implementation
     
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
-        self.request = request
+        self.req = request
         self.env = request.application.environment
         return next.respond(to: request)
     }
@@ -50,10 +53,10 @@ public final class AccountMiddleware: Middleware {
             parameter: accountId
         )
         
-        return request.client
+        return req.client
             .get(url) { $0.headers.contentType = .json }
             .flatMapThrowing { res in
-                self.request.logger.info(Logger.Message(stringLiteral: "GET \(url) -> \(res.status.code)"))
+                self.req.logger.info(Logger.Message(stringLiteral: "GET \(url) -> \(res.status.code)"))
                 guard res.status == .ok else { throw Abort(res.status) }
                 return try res.content.decode(CAS_Dto.Account.Res.self)
             }
@@ -68,12 +71,12 @@ public final class AccountMiddleware: Middleware {
     public func fetchStocks(
         tokens: [Blockchain.Token]
     ) throws -> EventLoopFuture<[Market_Dto.Agregate.Res]> {
-        guard let fiat = try? request.query.get(Fiat.self, at: "fiat") else {
+        guard let fiat = try? req.query.get(Fiat.self, at: "fiat") else {
             throw Abort(.badRequest)
         }
         
         let futures = try tokens.map {
-            try request
+            try req
                 .blockchain()
                 .market(
                     blockchainToken: $0,
@@ -81,7 +84,7 @@ public final class AccountMiddleware: Middleware {
                 )
         }
         
-        return request.client.eventLoop.flatten(futures).flatMapEachThrowing {
+        return self.req.client.eventLoop.flatten(futures).flatMapEachThrowing {
             .init(crypto: $0.crypto, fiat: $0.fiat, stock: $0.stock)
         }
     }
