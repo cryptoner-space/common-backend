532238bfc7715a18aea1e8ab06ed6625bd82d0c3
skibinalexander <skibinalexander@gmail.com>
2022-11-30 16:38:44 +0300

    update orders

--- Sources/CommonCryptoner/Data/Dto/cbs-ms/CBS+OrderDto.swift
+++ Sources/CommonCryptoner/Data/Dto/cbs-ms/CBS+OrderDto.swift
@@ -11,6 +11,63 @@ extension CBS_Dto {
     
     public struct Order {
         
+        public struct List {
+            
+            public struct Req: Content {
+                
+                /// Идентификатор владельца операции
+                public let ownerId: String
+                
+                // MARK: - Init
+                
+                public init(ownerId: String) {
+                    self.ownerId = ownerId
+                }
+                
+            }
+            
+            public struct Res: Content {
+                
+                /// Идентификатор владельца операции
+                public let ownerId: String
+                
+                /// Сотрудник обслуживающий операцию
+                public let employee: CBS_Dto.Employee.Res?
+                
+                /// Токен операции
+                public let token: Blockchain.Token
+                
+                /// Сумма операции
+                public let value: Int64
+                
+                /// Тип операции
+                public let type: CBS_Union.OrderTypeEnum
+                
+                /// Статус операции
+                public let status: CBS_Union.OrderStatusEnum
+                
+                // MARK: - Init
+                
+                public init(
+                    ownerId: String,
+                    employee: CBS_Dto.Employee.Res?,
+                    token: Blockchain.Token,
+                    value: Int64,
+                    type: CBS_Union.OrderTypeEnum,
+                    status: CBS_Union.OrderStatusEnum
+                ) {
+                    self.ownerId = ownerId
+                    self.employee = employee
+                    self.token = token
+                    self.value = value
+                    self.type = type
+                    self.status = status
+                }
+                
+            }
+            
+        }
+        
         public struct Create {
             
             public struct Req: Content {
