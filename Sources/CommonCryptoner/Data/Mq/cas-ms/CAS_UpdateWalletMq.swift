bb628f3dfcc70328acfec23b6fd2ee77e71869f7
skibinalexander <skibinalexander@gmail.com>
2022-12-05 12:23:28 +0300

    update

---
+++ Sources/CommonCryptoner/Data/Mq/cas-ms/CAS_UpdateWalletMq.swift
@@ -0,0 +1,38 @@
+//
+//  MQBase+CAS.swift
+//  
+//
+//  Created by skibinalexander on 02.12.2022.
+//
+
+import CommonVapor
+import Vapor
+
+extension CAS_Mq {
+    
+    public struct Update: Codable {
+        
+        /// Идентификатор кошелька
+        public let walletId: String
+        
+        /// Токен обнолвения баланса
+        public let token: Blockchain.Token
+        
+        /// Значение обновления баланса
+        public let value: Int64
+        
+        // MARK: - Init
+        
+        public init(
+            walletId: String,
+            token: Blockchain.Token,
+            value: Int64
+        ) throws {
+            self.walletId = walletId
+            self.token = token
+            self.value = value
+        }
+        
+    }
+    
+}
