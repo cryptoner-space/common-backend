5820839134cb47691fceab66cd649a35fc6e77dc
skibinalexander <skibinalexander@gmail.com>
2022-12-20 17:57:18 +0300

    update

---
+++ Sources/CommonCryptoner/Data/Unions/p2p-ms/P2P_Union_OrderType.swift
@@ -0,0 +1,31 @@
+//
+//  P2P_Union_OrderType.swift
+//  
+//
+//  Created by skibinalexander on 20.12.2022.
+//
+
+import Vapor
+import Fluent
+
+extension P2P_Union {
+    
+    /// Тип заявки сервиса P2P
+    public enum OrderTypeEnum: String, CaseIterable, Codable {
+        
+        // MARK: - Cases
+        
+        /// Продажа
+        case sale
+        
+        /// Покупка
+        case buy
+        
+    }
+    
+}
+
+extension P2P_Union.OrderTypeEnum {
+    static public let scheme: String = "p2p_order_type_enum_001"
+    static public let relation: FieldKey = .init(stringLiteral: "type")
+}
