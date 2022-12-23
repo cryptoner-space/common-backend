688af0be8291774bfe7e3cf7c1beecc7da21a15f
skibinalexander <skibinalexander@gmail.com>
2022-12-23 22:09:25 +0300

    update

---
+++ Sources/CommonCryptoner/Data/Dto/p2p-ms/P2P+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  P2P+Extensions.swift
+//  
+//
+//  Created by skibinalexander on 23.12.2022.
+//
+
+import Foundation
+
+extension Array where Element == Market_Dto.Agregate.Item {
+
+    public func find(by token: Blockchain.Token) throws -> Element {
+        guard let element = self.first(where: { $0.crypto.symbol == token.rawValue }) else {
+            throw Abort(.notFound)
+        }
+        
+        return element
+    }
+    
+}
