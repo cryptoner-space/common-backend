4368c99eb0453a12b5eb614207110d5963b964a4
skibinalexander <skibinalexander@gmail.com>
2022-12-27 18:43:15 +0300

    update

---
+++ Sources/CommonCryptoner/Data/Mq/chats-ms/Chats_Mq.swift
@@ -0,0 +1,14 @@
+//
+//  Chats_Mq.swift
+//  
+//
+//  Created by skibinalexander on 27.12.2022.
+//
+
+public struct Chats_Mq {
+    
+    public enum EventId: String {
+        case createMessage
+    }
+    
+}
