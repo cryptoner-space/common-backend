5868856017b1ef3d569afea4e37705f2bfaeca83
skibinalexander <skibinalexander@gmail.com>
2023-01-08 01:13:27 +0300

    update fiat payment union

---
+++ Sources/CommonCryptoner/Data/Unions/p2p-ms/P2P_Union_FiatPaymentType.swift
@@ -0,0 +1,27 @@
+//
+//  FiatPaymentType.swift
+//  
+//
+//  Created by skibinalexander on 08.01.2023.
+//
+
+import Vapor
+import Fluent
+
+extension P2P_Union {
+    
+    /// Способы получения платежа
+    public enum FiatPaymentType: String, CaseIterable, Codable {
+        
+        // MARK: - Cases
+        
+        case SBP, Tinkoff, Sberbank, VTB, YooMoney, Payeer, QIWI
+        
+    }
+    
+}
+
+extension P2P_Union.FiatPaymentType {
+    static public let scheme: String = "p2p_fiat_payment_type_enum_001"
+    static public let relation: FieldKey = .init(stringLiteral: "type")
+}
