75ab46773d78159c3690e78c33b423dc2a5d33db
skibinalexander <skibinalexander@gmail.com>
2023-01-08 01:28:26 +0300

    add region

---
+++ Sources/CommonCryptoner/Data/Unions/p2p-ms/P2P_Union_RegionType.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by skibinalexander on 08.01.2023.
+//
+
+import Vapor
+import Fluent
+
+extension P2P_Union {
+    
+    /// Способы получения платежа
+    public enum RegionType: String, CaseIterable, Codable {
+        
+        // MARK: - Cases
+        
+        case RU, UA
+        
+    }
+    
+}
+
+extension P2P_Union.RegionType {
+    static public let scheme: String = "p2p_region_type_enum_001"
+    static public let relation: FieldKey = .init(stringLiteral: "type")
+}
