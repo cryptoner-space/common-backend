7d808abb3680efbeb4c278d306ba56fdac9d7529
Skibin Alexander <skibinalexander@gmail.com>
2023-01-29 16:56:17 +0300

    update

--- Sources/CommonCryptoner/Data/Dto/Admin_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Admin_Dto+Content.swift
@@ -14,3 +14,6 @@ extension Admin_Dto.Device.Patch.Req: Content {}
 extension Admin_Dto.Device.Put.Req: Content {}
 extension Admin_Dto.User.Req: Content {}
 extension Admin_Dto.User.Res: Content {}
+extension Admin_Dto.Sign.Res: Content {}
+extension Admin_Dto.Sign.InReq: Content {}
+extension Admin_Dto.Sign.UpReq: Content {}
