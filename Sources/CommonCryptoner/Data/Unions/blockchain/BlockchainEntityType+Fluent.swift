8aa0ea0d9b39612a4c0cb891e3f341b1b01cf318
Skibin Alexander <skibinalexander@gmail.com>
2023-02-09 11:14:38 +0300

    Update enum blockchain

---
+++ Sources/CommonCryptoner/Data/Unions/blockchain/BlockchainEntityType+Fluent.swift
@@ -0,0 +1,14 @@
+//
+//  Blockchain.EntityType.swift
+//  
+//
+//  Created by skibinalexander on 07.02.2023.
+//
+
+import BlockchainSdk
+import Vapor
+import Fluent
+
+extension Blockchain.EntityType  {
+    static public let relation: FieldKey = .init(stringLiteral: "blockchain_entity_type")
+}
