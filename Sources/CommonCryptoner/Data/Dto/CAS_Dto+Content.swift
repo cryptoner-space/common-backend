06ed363a2a7d705a256b0125e0a873cca64fa808
Skibin Alexander <skibinalexander@gmail.com>
2023-02-12 13:21:40 +0300

    Update common cryptoner

--- Sources/CommonCryptoner/Data/Dto/CAS_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/CAS_Dto+Content.swift
@@ -11,8 +11,8 @@ import Vapor
 extension CAS_Dto.Account.Req: Content {}
 extension CAS_Dto.Account.Res: Content {}
 extension CAS_Dto.Wallet.Res: Content {}
-extension CAS_Dto.Wallet.Create.Req: Content {}
-extension CAS_Dto.Wallet.Create.Res: Content {}
+extension CAS_Dto.WalletItem.Req: Content {}
 extension CAS_Dto.WalletItem.Res: Content {}
+extension CAS_Dto.Wallet.Create.Req: Content {}
 extension CAS_Dto.Token.Push.Req: Content {}
 extension CAS_Dto.Token.Pull.Req: Content {}
