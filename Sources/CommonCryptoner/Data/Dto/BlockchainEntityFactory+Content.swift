8a353d0164a1efbd6b270ac6e6ca28b05e2163fd
Skibin Alexander <skibinalexander@gmail.com>
2023-02-26 00:06:01 +0300

    Add cryptoner data

---
+++ Sources/CommonCryptoner/Data/Dto/BlockchainEntityFactory+Content.swift
@@ -0,0 +1,11 @@
+//
+//  BlockchainEntityFactory+Content.swift
+//  
+//
+//  Created by skibinalexander on 26.02.2023.
+//
+
+import CommonCryptonerData
+import Vapor
+
+extension BlockchainEntityFactory.Entity: Content {}
