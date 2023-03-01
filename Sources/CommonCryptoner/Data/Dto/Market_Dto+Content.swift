4461440d18a7f0c08203e3119e10dd952f32fcc6
Skibin Alexander <skibinalexander@gmail.com>
2023-03-01 23:19:35 +0300

    Update

--- Sources/CommonCryptoner/Data/Dto/Market_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Market_Dto+Content.swift
@@ -11,6 +11,8 @@ import Vapor
 extension Market_Dto.Agregate.Req: Content {}
 extension Market_Dto.Agregate.Res: Content {}
 extension Market_Dto.Agregate.Item: Content {}
+extension Market_Dto.Growth.Req: Content {}
+extension Market_Dto.Growth.Res: Content {}
 extension Market_Dto.Currency.Crypto: Content {}
 extension Market_Dto.Currency.Fiat: Content {}
 extension Market_Dto.Stock: Content {}
