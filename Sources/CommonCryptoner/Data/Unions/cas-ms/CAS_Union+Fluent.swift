8dbddb0e42c5d56c6fceb73aad69a262c99f7b40
Skibin Alexander <skibinalexander@gmail.com>
2023-03-15 01:06:58 +0300

    Update unions

--- Sources/CommonCryptoner/Data/Unions/cas-ms/CAS_Union+Fluent.swift
+++ Sources/CommonCryptoner/Data/Unions/cas-ms/CAS_Union+Fluent.swift
@@ -5,12 +5,16 @@
 //  Created by skibinalexander on 06.12.2022.
 //
 
-import CommonCryptonerData
 import Vapor
 import Fluent
+import CommonCryptonerData
 
 extension CAS_Union.WalletType {
     static public let scheme: String = "wallet_type_enum_001"
     static public let relation: FieldKey = .init(stringLiteral: "wallet_type")
-    
+}
+
+extension CAS_Union.WalletLayout {
+    static public let scheme: String = "wallet_layout_enum_001"
+    static public let relation: FieldKey = .init(stringLiteral: "wallet_layout")
 }
