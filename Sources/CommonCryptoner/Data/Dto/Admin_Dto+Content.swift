e9b32176202f8df5c88c8579159d4996d1667450
Skibin Alexander <skibinalexander@gmail.com>
2023-04-01 17:22:23 +0300

    Update dto

--- Sources/CommonCryptoner/Data/Dto/Admin_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Admin_Dto+Content.swift
@@ -8,10 +8,9 @@
 import Vapor
 import CommonCryptonerData
 
-extension Admin_Dto.Device.Req: Content {}
 extension Admin_Dto.Device.Res: Content {}
+extension Admin_Dto.Device.Sign.Req: Content {}
 extension Admin_Dto.Device.Patch.Req: Content {}
-extension Admin_Dto.Device.Put.Req: Content {}
 extension Admin_Dto.User.Req: Content {}
 extension Admin_Dto.User.Res: Content {}
 extension Admin_Dto.Init.Res: Content {}
