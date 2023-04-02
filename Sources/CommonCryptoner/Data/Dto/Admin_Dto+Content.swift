9c92563dd569d9373636be7aa97e45099feca090
Skibin Alexander <skibinalexander@gmail.com>
2023-04-02 17:41:27 +0300

    Add cryptoner Admid_Dto

--- Sources/CommonCryptoner/Data/Dto/Admin_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Admin_Dto+Content.swift
@@ -15,6 +15,7 @@ extension Admin_Dto.User.Req: Content {}
 extension Admin_Dto.User.Res: Content {}
 extension Admin_Dto.Init.Res: Content {}
 extension Admin_Dto.SignIn.Init.Req: Content {}
+extension Admin_Dto.SignIn.Init.Res: Content {}
 extension Admin_Dto.SignUp.Init.Req: Content {}
 extension Admin_Dto.SignUp.Init.Res: Content {}
 extension Admin_Dto.Recovery.SetPassword.Req: Content {}
