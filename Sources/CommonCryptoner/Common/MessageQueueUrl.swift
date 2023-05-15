8c7c5d95720577286b93a8b835f4e7895a535464
Skibin Alexander <skibinalexander@gmail.com>
2023-05-15 20:09:44 +0400

    Update message queue url

--- Sources/CommonCryptoner/Common/MessageQueueUrl.swift
+++ Sources/CommonCryptoner/Common/MessageQueueUrl.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public enum MessageQueueUrl: String, CaseIterable {
     case market_external_mq = "dj600000000gh52500q0/core-market-ms-external-queue"
-    case swap_external_mq
-    case cbs_external_mq
+    case swap_external_mq = "dj600000000gif2m00q0/core-swap-ms-external-queue"
+    case cbs_external_mq = "dj600000000gif3200q0/core-cbs-ms-external-queu"
     case chat_internal_mq
 }
