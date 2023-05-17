16b4b966e60ce303e42bb70bf82288e80732130b
Skibin Alexander <skibinalexander@gmail.com>
2023-05-17 19:07:55 +0400

    Update resolved

--- Sources/CommonCryptoner/Data/Unions/cbs-ms/CBS_Union+Fluent.swift
+++ Sources/CommonCryptoner/Data/Unions/cbs-ms/CBS_Union+Fluent.swift
@@ -9,12 +9,55 @@ import Vapor
 import Fluent
 import CommonCryptonerData
 
-extension CBS_Union.OrderTypeEnum {
-    static public let scheme: String = "cbs_order_type_enum_001"
-    static public let relation: FieldKey = .init(stringLiteral: "type")
+extension CBS_Union.OrderType {
+    static public let schema: String = "cbs_order_type"
+    static public let relation: FieldKey = .init(stringLiteral: "order_type")
+    
+    public struct Migration_v001: Migration {
+        
+        public init() {}
+        
+        public func prepare(on database: Database) -> EventLoopFuture<Void> {
+            database
+                .enum(CBS_Union.OrderType.schema)
+                .case(CBS_Union.OrderType.DEPOSIT.rawValue)
+                .case(CBS_Union.OrderType.WITHDRAW.rawValue)
+                .create()
+                .transform(to: ())
+        }
+
+        public func revert(on database: Database) -> EventLoopFuture<Void> {
+            database
+                .enum(CBS_Union.OrderType.schema)
+                .delete()
+        }
+        
+    }
+
 }
 
-extension CBS_Union.OrderStatusEnum {
-    static public let scheme: String = "cbs_order_status_enum_001"
-    static public let relation: FieldKey = .init(stringLiteral: "status")
+extension CBS_Union.DepositoryLedgerType {
+    static let schema = "cbs_depositoty_leger_type"
+    static let relation: FieldKey = .init(stringLiteral: "ledger_type")
+    
+    public struct Migration_v001: Migration {
+        
+        public init() {}
+        
+        public func prepare(on database: Database) -> EventLoopFuture<Void> {
+            database
+                .enum(CBS_Union.DepositoryLedgerType.schema)
+                .case(CBS_Union.DepositoryLedgerType.DEPOSIT.rawValue)
+                .case(CBS_Union.DepositoryLedgerType.WITHDRAW.rawValue)
+                .create()
+                .transform(to: ())
+        }
+
+        public func revert(on database: Database) -> EventLoopFuture<Void> {
+            database
+                .enum(CBS_Union.DepositoryLedgerType.schema)
+                .delete()
+        }
+        
+    }
 }
