b30712d4726fe4be2f64551d9cbbfe6e36a53d13
Skibin Alexander <skibinalexander@gmail.com>
2023-06-18 02:27:37 +0300

    update fluent data

--- Sources/CommonCryptoner/Data/Unions/p2p-ms/P2P_Union_Fluent.swift
+++ Sources/CommonCryptoner/Data/Unions/p2p-ms/P2P_Union_Fluent.swift
@@ -9,17 +9,17 @@ import CommonCryptonerData
 import Vapor
 import Fluent
 
-extension P2P_Union.FiatPaymentTypeEnum {
-    static public let scheme: String = "p2p_fiat_payment_type_enum_001"
+extension P2P_Union.FiatPaymentType {
+    static public let scheme: String = "p2p_fiat_payment_type"
     static public let relation: FieldKey = .init(stringLiteral: "fiat_payment_type")
 }
 
-extension P2P_Union.OrderTypeEnum {
-    static public let scheme: String = "p2p_order_type_enum_001"
+extension P2P_Union.OrderType {
+    static public let scheme: String = "p2p_order_type"
     static public let relation: FieldKey = .init(stringLiteral: "order_type")
 }
 
 extension P2P_Union.RegionType {
-    static public let scheme: String = "p2p_region_type_enum_001"
+    static public let scheme: String = "p2p_region_type"
     static public let relation: FieldKey = .init(stringLiteral: "region_type")
 }
