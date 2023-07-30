f37d15a3d793ff6ab5463b2bd9e4ebb00ec1098e
Skibin Alexander <skibinalexander@gmail.com>
2023-07-30 18:57:39 +0300

    update

--- Sources/CommonCryptoner/Data/Dto/Swap_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Swap_Dto+Content.swift
@@ -8,7 +8,6 @@
 import Vapor
 import CommonCryptonerData
 
-extension Swap_Dto.Order.User.Init.Req: Content {}
 extension Swap_Dto.Order.User.Init.Res: Content {}
 extension Swap_Dto.Order.User.Create.Req: Content {}
 extension Swap_Dto.Order.Employee.Approve.Req: Content {}
