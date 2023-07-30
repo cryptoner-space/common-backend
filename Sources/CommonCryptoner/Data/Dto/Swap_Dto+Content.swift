50a91bf0d49e9466262ce75907c16b6aa588cc63
Skibin Alexander <skibinalexander@gmail.com>
2023-07-30 17:50:57 +0300

    update

--- Sources/CommonCryptoner/Data/Dto/Swap_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Swap_Dto+Content.swift
@@ -13,7 +13,7 @@ extension Swap_Dto.Order.User.Init.Res: Content {}
 extension Swap_Dto.Order.User.Create.Req: Content {}
 extension Swap_Dto.Order.Employee.Approve.Req: Content {}
 extension Swap_Dto.Order.Employee.List.Res: Content {}
-extension Swap_Dto.Exchange.Req: Content {}
+extension Swap_Dto.Order.Meta.Req: Content {}
+extension Swap_Dto.Order.Meta.Res: Content {}
 extension Swap_Dto.Exchange.Res: Content {}
-extension Swap_Dto.Exchange.Rate.Res: Content {}
 extension Swap_Dto.Currency.Res: Content {}
