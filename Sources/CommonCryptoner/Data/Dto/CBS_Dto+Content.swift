f55f6d0d39c4d265f5163d82628f9db4b2266370
Skibin Alexander <skibinalexander@gmail.com>
2023-08-01 00:13:41 +0300

    update cbs data

--- Sources/CommonCryptoner/Data/Dto/CBS_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/CBS_Dto+Content.swift
@@ -8,6 +8,8 @@
 import Vapor
 import CommonCryptonerData
 
-extension CBS_Dto.Order.Employee.List.Req: Content {}
-extension CBS_Dto.Order.Employee.List.Res: Content {}
+extension CBS_Dto.Currency.Res: Content {}
+extension CBS_Dto.Depository.Create.Req: Content {}
+extension CBS_Dto.Depository.List.Res: Content {}
+extension CBS_Dto.Depository.Item.Res: Content {}
 extension CBS_Dto.Employee.Res: Content {}
