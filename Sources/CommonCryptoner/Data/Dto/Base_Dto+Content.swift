cd79428ae8ffc102416463c1df8139325f5ebde6
Skibin Alexander <skibinalexander@gmail.com>
2023-08-26 14:42:32 +0300

    update dto

--- Sources/CommonCryptoner/Data/Dto/Base_Dto+Content.swift
+++ Sources/CommonCryptoner/Data/Dto/Base_Dto+Content.swift
@@ -10,7 +10,7 @@ import CommonCryptonerData
 
 extension BaseDto.Id.Req: Content {}
 extension BaseDto.Id.Res: Content {}
-extension BaseDto.Obj.Res: Content {}
+//extension BaseDto.Obj.Res: Content {}
 extension BaseDto.List.Res: Content {}
 extension BaseDto.List.Req: Content {}
 extension BaseDto.List.Group.Res: Content {}
