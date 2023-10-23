c15fd6e6fc146338e13549404fac0ee223f1b096
Skibin Alexander <skibinalexander@gmail.com>
2023-10-23 22:35:59 +0300

    Update lockbox service

--- Sources/CommonCryptoner/Middlewares/yandex/lockbox/LockboxService.swift
+++ Sources/CommonCryptoner/Middlewares/yandex/lockbox/LockboxService.swift
@@ -23,20 +23,20 @@ public struct LockboxService {
     
     // MARK: - Implementation
     
-    public func execute<P: LockboxPayload>(secretId: String) async throws -> P {
-        let res = try await app.client.get(
+    public func execute<P: LockboxPayload>(secretId: String) throws -> EventLoopFuture<P> {
+        app.client.get(
             .init(string: "https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/\(secretId)/payload")
         ) { req in
             req.headers.bearerAuthorization = .init(token: iam)
             req.headers.contentType = .json
+        }.flatMapThrowing { res in
+            guard res.status == .ok else {
+                throw Abort(.serviceUnavailable, reason: "Yandex Lockbox Service unavailable after request!")
+            }
+            
+            let result = try res.content.decode(LockboxResponse.self, using: JSONDecoder())
+            
+            return P(from: result)
         }
-        
-        guard res.status == .ok else {
-            throw Abort(.serviceUnavailable, reason: "Yandex Lockbox Service unavailable after request!")
-        }
-        
-        let result = try res.content.decode(LockboxResponse.self, using: JSONDecoder())
-        
-        return P(from: result)
     }
 }
