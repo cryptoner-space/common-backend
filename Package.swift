8eae966a25ff7d861f475fda709276d72f34a1f2
Skibin Alexander <skibinalexander@gmail.com>
2024-10-09 14:52:23 +0800

    COMMON-0002: Make refactor states

--- Package.swift
+++ Package.swift
@@ -15,13 +15,21 @@ let package = Package(
             targets: ["CommonBackend"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.84.6"),
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        // 🐘 Fluent driver for Postgres.
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
+        //
         .package(url: "https://github.com/vapor/jwt.git", from: "4.2.2"),
     ],
     targets: [
         .target(
             name: "CommonBackend",
             dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "JWT", package: "jwt"),
             ]
---
+++ Sources/CommonCryptoner/Middlewares/Yandex/lockbox/Lockbox.swift
@@ -0,0 +1,10 @@
+//
+//  ConfigDAO+.swift
+//
+//
+//  Created by skibinalexander on 23.10.2023.
+//
+
+import Foundation
+
+public enum Lockbox {}
