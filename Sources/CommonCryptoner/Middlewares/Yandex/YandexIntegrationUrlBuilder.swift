826c17b293f619ba132723a0fc60e21262c39aa2
Skibin Alexander <skibinalexander@gmail.com>
2024-10-24 19:14:38 +0300

    COMMON-0009 Make public yandex

--- Sources/CommonCryptoner/Middlewares/Yandex/YandexIntegrationUrlBuilder.swift
+++ Sources/CommonCryptoner/Middlewares/Yandex/YandexIntegrationUrlBuilder.swift
@@ -28,6 +28,10 @@ public struct YandexIntegrationUrlBuilder {
         case keys
     }
     
+    // MARK: - Init
+    
+    public init() {}
+    
     // MARK: - Implementation
     
     /// Доступ к сервису
