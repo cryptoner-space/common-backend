4de4dc549ab42b098a0127659553f3cc11b6ae2a
Skibin Alexander <skibinalexander@gmail.com>
2024-10-24 19:06:04 +0300

    COMMON-0004 Add yandex url builder

---
+++ Sources/CommonCryptoner/Middlewares/Yandex/YandexIntegrationUrlBuilder.swift
@@ -0,0 +1,56 @@
+//
+//  YandexIntegrationUrlBuilder.swift
+//
+//
+//  Created by Alexander Skibin on 23.10.2024.
+//
+
+import Foundation
+import Vapor
+
+/// Сервис конфигурации Yandex интеграции
+public struct YandexIntegrationUrlBuilder {
+    
+    /// Адрес интеграционного крипто сервиса
+    private let integration_host = "kms.yandex"
+    
+    /// Api доступ интеграционного сервиса
+    public enum ApiVersion: String {
+        case v1 = "v1"
+    }
+    
+    /// Набор сервис ендпоинтов
+    public enum Services: String {
+        case kms
+    }
+    
+    public enum Paths: String {
+        case keys
+    }
+    
+    // MARK: - Implementation
+    
+    /// Доступ к сервису
+    public func remoteUrl(
+        apiVersion: ApiVersion = .v1,
+        service: Services,
+        paths: String...,
+        query: String? = nil,
+        parameter: String? = nil
+        
+    ) -> URI {
+        URI(
+            scheme: .https,
+            host: integration_host,
+            port: nil,
+            path:
+                service.rawValue + "/" +
+                apiVersion.rawValue + "/" +
+                (paths.compactMap { $0 }
+                    .joined(separator: "/"))
+                    .replacingOccurrences(of: "//", with: "/")
+            ,
+            query: query
+        )
+    }
+}
